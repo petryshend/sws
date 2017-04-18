57b003fb4ad4cbcec92b0794e36c800a9ea811a1
petryshend <petryshend@gmail.com>
2017-04-18 10:57:50 +0300

    Add populate_db.sh script

---
+++ populate_db.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+php bin/console doctrine:database:drop --force
+php bin/console doctrine:database:create
+php bin/console doctrine:migrations:migrate -n
+php bin/console doctrine:fixtures:load -n
